2a6f78d6a1fd5eb547df07d566cc6da7245fd2c2
“mannlex21” <mannlexx21@gmail.com>
2022-04-21 01:35:19 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
-
+import axios from 'axios';
+import { Country } from './models/country.model';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-test-countries';
+  title = 'Countries';
+  username: string = '';
+  countries: Country[] = [];
+  
+  constructor(private http: HttpClient) { }
+  ngOnInit() {
+    this.searchCountry();
+  }
+  searchCountry(event?: any) {
+    let api = `https://restcountries.com/v3.1/all`;
+    if(event!=null && event.target.value != '' ){
+      api = `https://restcountries.com/v3.1/name/${event.target.value}`;
+    }
+
+    this.http.get<Array<Country>>(api).subscribe({
+      next: (data: Country[]) => {
+        console.log(data)
+        this.countries = data;
+      },
+      error: (error: any) => {
+        console.log(error);
+      }
+    });
+  }
+
+  loginImg(url: string) {
+    return this.http.get(url, { responseType: 'blob' });
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
